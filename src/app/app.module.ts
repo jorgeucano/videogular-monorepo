ef508635e5f5afde0bda084c61d3fb24203e08dd
Jorge Cano <jorgeucano@gmail.com>
2017-12-12 19:44:44 -0300

    testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// videogular
+import { CoreModule } from './modules/core/core.module';
+import { ControlsModule } from './modules/controls/controls.module';
+import { BufferingModule } from './modules/buffering/buffering.module';
+import { ImaAdsModule } from './modules/ima-ads/ima-ads.module';
+import { OverlayPlayModule } from './modules/overlay-play/overlay-play.module';
+import { StreamingModule } from './modules/streaming/streaming.module';
+
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +18,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    ControlsModule,
+    BufferingModule,
+    ImaAdsModule,
+    OverlayPlayModule,
+    StreamingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/streaming/streaming.ngfactory.d.ts
@@ -0,0 +1,3 @@
+import * as i0 from '@angular/core';
+import * as i1 from '../streaming/streaming.module';
+export declare const VgStreamingModuleNgFactory: i0.NgModuleFactory<i1.StreamingModule>;
